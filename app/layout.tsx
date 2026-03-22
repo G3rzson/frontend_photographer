ca10a7a25dd29c441c1fd09e0f458d6dcf6c844a
G3rzson <gerzonpal@gmail.com>
2026-03-22 12:36:17 +0100

    feat: create header with nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Header from "@/features/Header/Header";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
