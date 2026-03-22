58744020944bdf05cb9b2b7a5218377470499b15
G3rzson <gerzonpal@gmail.com>
2026-03-22 16:49:17 +0100

    fix: added projects data

---
+++ Constants/constants.ts
@@ -0,0 +1,39 @@
+export const NAV_LINKS = [
+  {
+    href: "/works",
+    title: "Munkák",
+  },
+  {
+    href: "/prices",
+    title: "Árak",
+  },
+  {
+    href: "/contacts",
+    title: "Kapcsolatok",
+  },
+];
+
+export const PROJEKTS = [
+  {
+    title: "Eskövői fotók",
+    images: [
+      { src: "/wedding/wedding_1.png", alt: "Eskövői fotó 1" },
+      { src: "/wedding/wedding_2.png", alt: "Eskövői fotó 2" },
+      { src: "/wedding/wedding_3.png", alt: "Eskövői fotó 3" },
+      { src: "/wedding/wedding_4.png", alt: "Eskövői fotó 4" },
+      { src: "/wedding/wedding_5.png", alt: "Eskövői fotó 5" },
+      { src: "/wedding/wedding_6.png", alt: "Eskövői fotó 6" },
+    ],
+  },
+  {
+    title: "Születésnapi fotók",
+    images: [
+      { src: "/birthday/birthday_1.png", alt: "Születésnapi fotó 1" },
+      { src: "/birthday/birthday_2.png", alt: "Születésnapi fotó 2" },
+      { src: "/birthday/birthday_3.png", alt: "Születésnapi fotó 3" },
+      { src: "/birthday/birthday_4.png", alt: "Születésnapi fotó 4" },
+      { src: "/birthday/birthday_5.png", alt: "Születésnapi fotó 5" },
+      { src: "/birthday/birthday_6.png", alt: "Születésnapi fotó 6" },
+    ],
+  },
+];
